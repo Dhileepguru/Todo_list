bed7fdfc9a2a33b40650ab46f8b7b75986ccfff0
Dhileepguru <dhileepguru497@gmail.com>
2025-01-02 09:24:35 +0530

    Created local storage

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,41 @@
 var text=document.getElementById("inputid");
 var list=document.getElementById("todolist");
 let arr=[];
+window.onload=()=>
+{
+ arr=JSON.parse(localStorage.getItem("value"))|| [];
+ arr.forEach((ele)=>{
+   displaystore(ele)
+ })
+}
 function display()
 {
-    arr.push(text.value);
-    listDisplay(text.value);
+    if(text.value.trim() !="") 
+    {
+      arr.push(text.value);
+      let id=arr.length-1;
+      liveele(arr,id);
+      localStorage.setItem("value",JSON.stringify(arr));
+      text.value=" ";
+    }    
 }
-function listDisplay(txt)
+function liveele(arr,id)
 {
-  let para=document.createElement("p");
-  para.innerText=txt;
-  list.appendChild(para);
+  list.innerHTML +=` <li style="margin-left=-100px;" id="${id}">${text.value.trim()} <button style="margin:10px;" onclick="deletemethod(${id})">Delete</button> </li>`
+}
+function displaystore(arr,id)
+{
+  list.innerHTML +=` <li style="margin-left=-100px;" id="${id}">${arr} <button style="margin:10px;" onclick="deleteold(${id})">Delete</button> </li>`
+}
+
+function deleteold(id)
+{
+ 
+}
+function deletemethod(id)
+{ 
+  console.log("clicked");
+  let item=document.getElementById(id);
+  item.style.display= "none";
+  //localStorage.setItem("value",JSON.stringify(arr));
 }
